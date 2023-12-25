8c20517568118123462f903ba16488179c50f40b
dssff <dendssff@gmail.com>
2023-12-26 01:03:12 +0200

    main

---
+++ Lab14 task2/Lab14 task2/Lab14 task2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+    // створеюємо масив
+    int arr[7];
+    int* ptr = arr; 
+    double sum = 0;
+    //заповнюємо масив елементами які вводить користувач використовуючи вказівники
+    printf("enter array element:\n");
+    for (int i = 0; i < 7; i++) {
+        scanf_s("%d", ptr + i); 
+        sum += *(ptr + i);     
+    }
+
+    // виводимо середнє значення
+    double average = sum / 7;
+    printf("The average value of the array elements: %.2f\n", average);
+}
---
+++ lab14 task1/lab14 task1/lab14 task1.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int arr[15];
+    srand(time(NULL)); // ініціалізація генерації випадкових чисел використовуючи час 
+    printf("array elements:\n");
+    for (int i = 0; i < 15; i++) {
+        arr[i] = rand() % 250+ 1; // заповнюємо масив рандомними числами від 250 до 1
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    // знаходження максимального та мінімального значення через проходження по кожному елементу
+    int* min = arr;
+    int* max = arr;
+    for (int i = 1; i < 15; i++) {
+        if (arr[i] < *min) {
+            min = &arr[i];
+        }
+        if (arr[i] > *max) {
+            max = &arr[i];
+        }
+    }
+
+    // Виводимо мінімальний та максимальний елементи та їхні позиції
+    printf("minimal array element: %d, position: %ld\n", *min, min - arr + 1 );
+    printf("maximal array element: %d, position: %ld\n", *max, max - arr + 1);
+
+    // Виводимо відстань між числами використовуючи модуль 
+    printf("distance between elements : %ld\n", labs(max - min));   
+
+    return 0;
+}
